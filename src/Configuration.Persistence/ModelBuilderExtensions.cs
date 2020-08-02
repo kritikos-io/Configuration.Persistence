2f91d1350a3ee394f3d3dbf797f66bc8c9f43863
Alexandros Kritikos <akritikos@outlook.com>
2020-08-02 11:59:12 +0300

    :recycle: Simplifies ModelBuilderExtensions

--- src/Configuration.Persistence/ModelBuilderExtensions.cs
+++ src/Configuration.Persistence/ModelBuilderExtensions.cs
@@ -12,19 +12,14 @@ namespace Kritikos.Configuration.Persistence
 			this ModelBuilder modelBuilder,
 			Action<EntityTypeBuilder> buildAction)
 			where T : class
-			=> modelBuilder.EntitiesOfType(typeof(T), buildAction);
-
-		private static ModelBuilder EntitiesOfType(
-			this ModelBuilder modelBuilder,
-			Type type,
-			Action<EntityTypeBuilder> buildAction)
 		{
 			if (modelBuilder == null)
 			{
 				throw new ArgumentNullException(nameof(modelBuilder));
 			}
 
-			foreach (var entityType in modelBuilder.Model.GetEntityTypes().Where(x => type.IsAssignableFrom(x.ClrType)))
+			foreach (var entityType in modelBuilder.Model.GetEntityTypes()
+				.Where(x => typeof(T).IsAssignableFrom(x.ClrType)))
 			{
 				buildAction(modelBuilder.Entity(entityType.ClrType));
 			}
