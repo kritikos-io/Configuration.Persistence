20b8dd58f07a4d133cad51c244f9471f65a38078
Alexandros Kritikos <akritikos@outlook.com>
2021-02-12 15:45:35 +0200

    Fixes analyzer suppressions

--- src/Configuration.Persistence/Base/ConcurrentEntity.cs
+++ src/Configuration.Persistence/Base/ConcurrentEntity.cs
@@ -1,3 +1,4 @@
+#nullable disable
 namespace Kritikos.Configuration.Persistence.Base
 {
 	using System;
@@ -13,16 +14,12 @@ namespace Kritikos.Configuration.Persistence.Base
 		/// Primary Key of entity.
 		/// </summary>
 		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-#pragma warning disable CS8618 // Primary key should be handled by database.
 		public TKey Id { get; set; }
-#pragma warning restore CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
 
 		/// <summary>
 		/// Field used to track database concurrency hits.
 		/// </summary>
 		[Timestamp]
-#pragma warning disable CA1819 // Proposed way to handle database concurrency in SQL Server.
 		public byte[] RowVersion { get; set; } = Array.Empty<byte>();
-#pragma warning restore CA1819 // Properties should not return arrays
 	}
 }
