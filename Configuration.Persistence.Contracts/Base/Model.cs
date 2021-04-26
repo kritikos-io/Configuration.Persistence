953ec3da472c84a8e882abe92938c18370451cd8
Alexandros Kritikos <akritikos@outlook.com>
2021-04-26 12:00:31 +0300

    ✨ Adds a simple model for DTOs for usage with Observables

---
+++ Configuration.Persistence.Contracts/Base/Model.cs
@@ -0,0 +1,31 @@
+namespace Kritikos.Configuration.Persistence.Contracts.Base
+{
+  using System;
+  using System.Collections.Generic;
+
+  using Kritikos.Configuration.Persistence.Contracts.Behavioral;
+
+  /// <summary>
+  /// Base model for Dtos to allow comparsions after updating from the server.
+  /// </summary>
+  /// <typeparam name="TKey">Type of primary identity.</typeparam>
+  public abstract class Model<TKey> : IEntity<TKey>, IEquatable<Model<TKey>>
+    where TKey : IEquatable<TKey>, IComparable<TKey>, IComparable
+  {
+    public TKey Id { get; set; } = default!;
+
+    /// <inheritdoc />
+    public virtual bool Equals(Model<TKey>? other)
+      => other is not null
+         && (ReferenceEquals(this, other) || EqualityComparer<TKey>.Default.Equals(this.Id, other.Id));
+
+    /// <inheritdoc />
+    public override bool Equals(object? obj)
+      => obj is not null
+         && (ReferenceEquals(this, obj) || (obj is Model<TKey> m && Equals(m)));
+
+    /// <inheritdoc />
+    public override int GetHashCode()
+      => EqualityComparer<TKey>.Default.GetHashCode(Id);
+  }
+}
