645f118b5fd3469dedb6b54289441df8e690208f
Alexandros Kritikos <akritikos@outlook.com>
2021-05-05 14:25:15 +0300

    🚚 Finishes project separation

--- src/Configuration.Persistence.Contracts/Behavioral/IEntity.cs
+++ src/Configuration.Persistence.Contracts/Behavioral/IEntity.cs
@@ -1,12 +1,20 @@
+#pragma warning disable SA1402 // File may only contain a single type
 namespace Kritikos.Configuration.Persistence.Contracts.Behavioral
 {
   using System;
 
   /// <summary>
+  /// Marker interface used to decorate join entities.
+  /// </summary>
+  public interface IEntity
+  {
+  }
+
+  /// <summary>
   /// Exposes basic behavior for relational entity persistence.
   /// </summary>
   /// <typeparam name="TKey">Type of primary key.</typeparam>
-  public interface IEntity<TKey>
+  public interface IEntity<TKey> : IEntity
     where TKey : IComparable, IComparable<TKey>, IEquatable<TKey>
   {
     TKey Id { get; set; }
---
+++ src/Configuration.Persistence/Extensions/HostExtensions.cs
@@ -0,0 +1,66 @@
+namespace Kritikos.Configuration.Persistence.Extensions
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Threading;
+  using System.Threading.Tasks;
+
+  using Microsoft.EntityFrameworkCore;
+  using Microsoft.Extensions.DependencyInjection;
+  using Microsoft.Extensions.Hosting;
+  using Microsoft.Extensions.Logging;
+
+  public static class HostExtensions
+  {
+    /// <summary>
+    /// Applies pending migrations on <typeparamref name="TDbContext"/>.
+    /// </summary>
+    /// <typeparam name="TDbContext">The <see cref="DbContext"/> to migrate.</typeparam>
+    /// <param name="host">The <see cref="IHost"/> to operate on.</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken"/> to observe while waiting for the task to complete.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous migration operation.</returns>
+    public static async Task MigrateAsync<TDbContext>(this IHost host, CancellationToken cancellationToken = default)
+      where TDbContext : DbContext
+    {
+      using var scope = host.Services.CreateScope();
+      var contextName = typeof(TDbContext).Name;
+
+      var ctx = scope.ServiceProvider.GetService<TDbContext>();
+      if (ctx == null)
+      {
+        throw new InvalidOperationException($"Could not resolve an instance of {contextName}");
+      }
+
+      var logger = scope.ServiceProvider.GetRequiredService<ILogger<TDbContext>>();
+
+      var migrations = (await ctx.Database.GetPendingMigrationsAsync(cancellationToken)).ToList();
+      if (migrations.Any())
+      {
+        logger.LogInformation("Applying migrations to {DbContext}: {Migrations}", contextName, migrations);
+        await ctx.Database.MigrateAsync(cancellationToken);
+        logger.LogInformation("Migrations for {DbContext} completed succesfully", contextName);
+      }
+    }
+
+    public static async Task<int> InsertSeedData<TDbContext>(
+      this IHost host,
+      IEnumerable<object> entities,
+      CancellationToken cancellationToken = default)
+      where TDbContext : DbContext
+    {
+      using var scope = host.Services.CreateScope();
+      var contextName = typeof(TDbContext).Name;
+
+      var ctx = scope.ServiceProvider.GetService<TDbContext>();
+      if (ctx == null)
+      {
+        throw new InvalidOperationException($"Could not resolve an instance of {contextName}");
+      }
+
+      ctx.AddRange(entities);
+
+      return await ctx.SaveChangesAsync(cancellationToken);
+    }
+  }
+}
