658e04d7e15944fb2078538ac56df1db2dff6970
Alexandros Kritikos <akritikos@outlook.com>
2026-08-30 01:46:21 +0300

    🧪 Updates tests for redacted columns

---
+++ samples/Samples.CityCensus/Migrations/20260829222022_AddsRedactedAuditColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kritikos.Samples.CityCensus.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddsRedactedAuditColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Redacted",
+                table: "AuditRecords",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "[]");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Redacted",
+                table: "AuditRecords");
+        }
+    }
+}
